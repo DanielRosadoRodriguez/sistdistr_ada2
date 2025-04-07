cb048e74bbaac7cf2cc0c5de33ef7f9adf7f73d7
Daniel Rosado <rosado.rodriguez.daniel93@gmail.com>
2025-04-07 11:28:19 -0600

    fix: rename endpoint producto -> product

--- src/main/java/com/moo_rest/spring_app/controllers/ProductController.java
+++ src/main/java/com/moo_rest/spring_app/controllers/ProductController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequestMapping("/producto")
+@RequestMapping("/product")
 public class ProductController {
     
     @Autowired
